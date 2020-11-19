8a48dcd20b058f18fdc6e6a5c7181df0decb00ac
Nitishkumar Singh <nitishkumarsingh71@gmail.com>
2020-11-19 08:44:04 +0530

    included rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
     postsPerPage: 10,
     showMenuItems: 2,
     menuMoreText: 'Show more',
+    siteUrl: 'https://blog.nitishkumarsingh.xyz',
     mainMenu: [
       {
         title: 'About',
@@ -139,6 +140,59 @@ module.exports = {
     `gatsby-plugin-nprogress`,
     `gatsby-plugin-polyfill-io`,
     'gatsby-plugin-draft',
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Nitishkumar's Blog",
+          },
+        ],
+      },
+    }
     // {
     //   resolve: "gatsby-plugin-guess-js",
     //   options: {
